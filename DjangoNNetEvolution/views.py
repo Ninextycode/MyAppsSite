26d4b1a65f28b8478e6f99eed76ebf19f8a41502
maxim <ninexty@gmail.com>
2017-06-13 01:38:43 +0300

    Html tags fixed

--- DjangoNNetEvolution/views.py
+++ DjangoNNetEvolution/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 
 # Create your views here.
 
 def main_view(request):
-    return render(request, 'main_nnev.html')
+    return render_to_response('main_nnev.html')
 
